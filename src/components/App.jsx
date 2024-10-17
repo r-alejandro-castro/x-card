b13563eb03dcd49f45d40b94f1f4195de8df3eee
Rodolfo Castro <soyracm@gmail.com>
2024-10-17 15:29:12 -0400

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+export  function App() { 
+  return (
+    <>
+      <article>
+        <header>
+          <img src="https://unavatar.io/veggeta777" alt="avatar jhon wick" />
+          <div>
+            <strong>Jhon Wick</strong>
+            <span>@jhonwick</span>
+          </div>
+        </header>
+
+        <aside>
+          <button>
+            Seguir
+          </button>
+        </aside>
+      </article>
+    </>
+  )
+}
